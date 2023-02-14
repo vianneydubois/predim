f9aef3ece3b5747fa35e6eaf694367bbda25701e
vianneydubois <vianneydubois@hotmail.fr>
2023-02-14 11:51:53 +0100

    input.py file with predim values

---
+++ input.py
@@ -0,0 +1,17 @@
+# FUSELAGE
+fus_width = 0.25
+
+
+# WEIGHT
+MTOW = 24
+# LIFT REQUIREMENTS
+w_C_L_max_target = 2.1
+
+# WING
+w_S = 2.1
+w_span = 4.2
+w_chord = 0.50
+
+# AEROFOIL (E423)
+# max aeroifoil lift coefficient at Re~300k
+w_C_l_max_clean = 1.9 # from Selig
